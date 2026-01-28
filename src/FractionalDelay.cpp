89b84f4b7257655253f4950b05a071e994e500a6
Chris Wade-Evans <wevans@cix.co.uk>
2026-01-28 13:53:20 +0000

    Add skeleton functions to FractionalDelay class

--- src/FractionalDelay.cpp
+++ src/FractionalDelay.cpp
@@ -1,1 +1,18 @@
 #include "FractionalDelay.h"
+
+void FractionalDelay::prepare(double sampleRate, int maxDelayInSamples)
+{
+}
+
+void FractionalDelay::setDelay(float delayMs)
+{
+}
+
+float FractionalDelay::processSample(float input)
+{
+    return input;  // Pass-through for now
+}
+
+void FractionalDelay::clear()
+{
+}
--- src/FractionalDelay.h
+++ src/FractionalDelay.h
@@ -6,5 +6,10 @@ public:
     FractionalDelay() = default;
     ~FractionalDelay() = default;
 
+    void prepare(double sampleRate, int maxDelayInSamples);
+    void setDelay(float delayMs);
+    float processSample(float input);
+    void clear();
+
 private:
 };
