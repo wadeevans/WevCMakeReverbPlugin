cada41892b8fc71c11c94705061af3c0ed609a75
Chris Wade-Evans <wevans@cix.co.uk>
2026-03-02 17:48:25 +0000

    Add IntegerFBCF class

---
+++ src/IntegerFBCF.cpp
@@ -0,0 +1,38 @@
+// IntegerFBCF.cpp
+// Created by CWE 02/03/26
+
+#include "IntegerFBCF.h"
+
+void IntegerFBCF::prepare(double sampleRate, int delayInSamples, float feedbackGain)
+{
+    m_feedbackGain = feedbackGain;
+    m_delayLine.prepare(delayInSamples);
+
+    m_dampingFilter.prepare(sampleRate);
+    m_dampingFilter.setCutoffFrequency(10000.0f);
+
+    clear();
+}
+
+float IntegerFBCF::processSample(float input)
+{
+    // y(n) = x(n) + g * y(n-M)
+    float dampedOutput;
+
+    if (m_dampingEnabled)
+        dampedOutput = m_dampingFilter.processSample(m_delayOutput);
+    else
+        dampedOutput = m_delayOutput;  // TRUE BYPASS
+
+    float output = input + (dampedOutput * m_feedbackGain);
+    m_delayOutput = m_delayLine.processSample(output);
+
+    return m_delayOutput;
+}
+
+void IntegerFBCF::clear()
+{
+    m_delayLine.clear();
+    m_dampingFilter.clear();
+    m_delayOutput = 0.0f; // Also reset the output
+}
---
+++ src/IntegerFBCF.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "IntegerDelay.h"
+#include "OnePoleFilter.h"
+
+class IntegerFBCF
+{
+public:
+    void prepare(double sampleRate, int delayInSamples, float feedbackGain = 0.7f);
+    float processSample(float input);
+    void clear();
+
+
+private:
+    float m_feedbackGain = 0.7f;
+    float m_delayOutput = 0.0f;
+    bool m_dampingEnabled = false;
+    
+
+    IntegerDelay m_delayLine;
+    OnePoleFilter m_dampingFilter;
+};
